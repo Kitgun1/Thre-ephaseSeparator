2b764c788fd9cc2d6eb3a0c9caa1ea9d5fbd9a38
Kimicu <kitgunplay@gmail.com>
2023-05-12 17:27:40 +0300

    add interfaces for Quest System

---
+++ Assets/_Project/Scripts/Systems/Quest/IQuest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Project.Scripts.Systems.Quest
+{
+    public interface IQuest
+    {
+        public event Action<IQuest> OnComplete;
+        public event Action<IQuest> OnUncomplete;
+
+        public void InitListener();
+        public List<TaskInfo> GetTasks();
+    }
+}
---
+++ Assets/_Project/Scripts/Systems/Quest/ITaskQuest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _Project.Scripts.Systems.Quest
+{
+    public interface ITaskQuest
+    {
+        public event Action<ITaskQuest> OnComplete;
+        public event Action<ITaskQuest> OnUncomplete;
+    }
+}
