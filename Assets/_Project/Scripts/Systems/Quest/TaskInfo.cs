1367ce28e7434663d7a644406bb628a0ee5e1ba9
Kimicu <kitgunplay@gmail.com>
2023-05-12 17:41:33 +0300

    no message

---
+++ Assets/_Project/Scripts/Systems/Quest/TaskInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KimicuUtilities.Attributes;
+using UnityEngine;
+
+namespace _Project.Scripts.Systems.Quest
+{
+    [Serializable]
+    public struct TaskInfo
+    {
+        public string Name;
+        public string Description;
+        [RequireInterface(typeof(ITaskQuest))] public List<GameObject> TaskObjects;
+
+        public List<ITaskQuest> GetTasks()
+        {
+            return TaskObjects.Select(taskObject => taskObject.GetComponent<ITaskQuest>()).ToList();
+        }
+    }
+}
